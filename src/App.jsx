18b3db0b10765ee4acf3ac3005e56c6d2f6a9c81
chrisconover83 <116586935+chrisconover83@users.noreply.github.com>
2023-06-06 22:42:53 -0500

    Vanlife project using React Router

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import {BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
+import "./server"
+
+
+const App = () => {
+  return (
+    <Router>
+      <header>
+          <Link className="site-logo" to="/">#VanLife</Link>
+          <nav>
+            <Link to="/about">About</Link>
+            <Link to="/vans">Vans</Link>
+          </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
